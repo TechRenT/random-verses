994f25c13fbd19cacb9d2da6c891c9937fe6a9c5
Renelle Tigue <titingrenelle@gmail.com>
2019-03-19 19:42:58 +0800

    Simple working version

--- script/main.js
+++ script/main.js
@@ -1,3 +1,9 @@
-var message = verses[0].verse;
-console.log(message);
+function getRandomVerse(list) {
+  var randomNumber = Math.floor( Math.random() * list.length );
+  return list[randomNumber];
+}
+
+var randomVerse = getRandomVerse(verses);
+var message = "<h2>" + randomVerse.verse + "</h2>";
+message += "<p>" + randomVerse.text + "</p>";
 document.write(message);
--- script/verses.js
+++ script/verses.js
@@ -1,11 +1,22 @@
 var verses = [
   {
-    verse: "For I know the plans I have for you,\” declares the Lord, \“plans to prosper you and not to harm you, plans to give you hope and a future."
+    verse: "Jeremiah 29:11",
+    text: "For I know the plans I have for you, \” declares the Lord, \“plans to prosper you and not to harm you, plans to give you hope and a future."
   },
   {
-    verse: "But seek first his kingdom and his righteousness, and all these things will be given to you as well."
+    verse: "Matthew 6:33",
+    text: "But seek first his kingdom and his righteousness, and all these things will be given to you as well."
   },
   {
-    verse: "So do not fear, for I am with you; do not be dismayed, for I am your God. I will strengthen you and help you; I will uphold you with my righteous right hand."
+    verse: "Isaiah 41:10",
+    text: "So do not fear, for I am with you; do not be dismayed, for I am your God. I will strengthen you and help you; I will uphold you with my righteous right hand."
+  },
+  {
+    verse: "Proverbs 10:22",
+    text: "The blessing of the Lord makes rich, and he adds no sorrow with it."
+  },
+  {
+    verse: "Hebrews 13:5",
+    text: "Keep your life free from love of money, and be content with what you have, for he has said, \“I will never leave you nor forsake you.\”"
   }
 ]
