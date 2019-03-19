ef125bc481cf911a74475caae49364ab59378b14
Renelle Tigue <titingrenelle@gmail.com>
2019-03-19 16:45:36 +0800

    Initial commit

---
+++ script/main.js
@@ -0,0 +1,3 @@
+var message = verses[0].verse;
+console.log(message);
+document.write(message);
---
+++ script/verses.js
@@ -0,0 +1,11 @@
+var verses = [
+  {
+    verse: "For I know the plans I have for you,\” declares the Lord, \“plans to prosper you and not to harm you, plans to give you hope and a future."
+  },
+  {
+    verse: "But seek first his kingdom and his righteousness, and all these things will be given to you as well."
+  },
+  {
+    verse: "So do not fear, for I am with you; do not be dismayed, for I am your God. I will strengthen you and help you; I will uphold you with my righteous right hand."
+  }
+]
