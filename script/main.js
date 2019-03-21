67d9934205c9e1bf08796f51f6590c8e04c141a4
Renelle Tigue <titingrenelle@gmail.com>
2019-03-22 03:14:15 +0800

    create a new random verse that is different from the previous verse

--- script/main.js
+++ script/main.js
@@ -1,20 +1,34 @@
-function getRandomVerse(list) {
-  var randomNumber = Math.floor( Math.random() * list.length );
-  return list[randomNumber];
+//generate a random number
+function randomNumber() {
+  let randomNum = Math.floor( Math.random() * verses.length );
+  return randomNum;
 }
 
+//generate a random verse that is different from the previous verse
+function getRandomVerse() {
+  let randomNum = randomNumber();
+  while (previousRandomNumber === randomNum) {
+    randomNum = randomNumber();
+  }
+  previousRandomNumber = randomNum;
+  return verses[randomNum];
+}
+
+//print message to the div with the class verse-container
 function print(message) {
   var outputDiv = document.getElementById('verse-container');
   outputDiv.innerHTML = message;
 }
 
+//process the random verse, create a message and print it to the div
 function renderVerse() {
-  var randomVerse = getRandomVerse(verses);
+  var randomVerse = getRandomVerse();
   var message = "<h2>" + randomVerse.verse + "</h2>";
   message += "<p>" + randomVerse.text + "</p>";
   print(message);
 }
 
+var previousRandomNumber;
 renderVerse();
 const myButton = document.getElementById('verse-button');
 myButton.addEventListener('click', renderVerse);
