308aa0e44ee85edea7c1974f3fad4cf2fb4e05b4
Renelle Tigue <titingrenelle@gmail.com>
2019-04-03 15:09:18 +0800

    Added encouraging verses

--- script/verses.js
+++ script/verses.js
@@ -1,22 +1,86 @@
 var verses = [
   {
-    verse: "Jeremiah 29:11",
-    text: "For I know the plans I have for you, \” declares the Lord, \“plans to prosper you and not to harm you, plans to give you hope and a future."
+    verse: "1 Corinthians 10:13",
+    text: "No temptation has overtaken you that is not common to man. God is faithful, and he will not let you be tempted beyond your ability, but with the temptation he will also provide the way of escape, that you may be able to endure it."
   },
   {
-    verse: "Matthew 6:33",
-    text: "But seek first his kingdom and his righteousness, and all these things will be given to you as well."
+    verse: "2 Corinthians 12:9-10",
+    text: "But he said to me, “My grace is sufficient for you, for my power is made perfect in weakness.” Therefore I will boast all the more gladly of my weaknesses, so that the power of Christ may rest upon me. For the sake of Christ, then, I am content with weaknesses, insults, hardships, persecutions, and calamities. For when I am weak, then I am strong."
+  },
+  {
+    verse: "2 Timothy 1:7",
+    text: "For God gave us a spirit not of fear but of power and love and self-control."
+  },
+  {
+    verse: "Exodus 15:2",
+    text: "The Lord is my strength and my song, and he has become my salvation; this is my God, and I will praise him, my father's God, and I will exalt him."
+  },
+  {
+    verse: "Deuteronomy 20:4",
+    text: "For the Lord your God is he who goes with you to fight for you against your enemies, to give you the victory."
+  },
+  {
+    verse: "Deuteronomy 31:6",
+    text: "Be strong and courageous. Do not fear or be in dread of them, for it is the Lord your God who goes with you. He will not leave you or forsake you."
+  },
+  {
+    verse: "Hebrews 13:5",
+    text: "Keep your life free from love of money, and be content with what you have, for he has said, \“I will never leave you nor forsake you.\”"
+  },
+  {
+    verse: "Isaiah 12:2",
+    text: "Behold, God is my salvation; I will trust, and will not be afraid; for the Lord God is my strength and my song, and he has become my salvation."
+  },
+  {
+    verse: "Isaiah 40:29",
+    text: "He gives power to the faint, and to him who has no might he increases strength.",
+  },
+  {
+    verse: "Isaiah 40:31",
+    text: "But they who wait for the Lord shall renew their strength; they shall mount up with wings like eagles; they shall run and not be weary; they shall walk and not faint."
   },
   {
     verse: "Isaiah 41:10",
     text: "So do not fear, for I am with you; do not be dismayed, for I am your God. I will strengthen you and help you; I will uphold you with my righteous right hand."
   },
   {
-    verse: "Proverbs 10:22",
-    text: "The blessing of the Lord makes rich, and he adds no sorrow with it."
+    verse: "Jeremiah 29:11",
+    text: "For I know the plans I have for you, \” declares the Lord, \“plans to prosper you and not to harm you, plans to give you hope and a future."
   },
   {
-    verse: "Hebrews 13:5",
-    text: "Keep your life free from love of money, and be content with what you have, for he has said, \“I will never leave you nor forsake you.\”"
+    verse: "John 16:33",
+    text: "I have said these things to you, that in me you may have peace. In the world you will have tribulation. But take heart; I have overcome the world.",
+  },
+  {
+    verse: "Joshua 1:9",
+    text: "Have I not commanded you? Be strong and courageous. Do not be frightened, and do not be dismayed, for the Lord your God is with you wherever you go."
+  },
+  {
+    verse: "Mark 12:30",
+    text: "And you shall love the Lord your God with all your heart and with all your soul and with all your mind and with all your strength.’",
+  },
+  {
+    verse: "Matthew 6:33",
+    text: "But seek first his kingdom and his righteousness, and all these things will be given to you as well."
+  },
+  {
+    verse: "Matthew 11:28",
+    text: "Come to me, all who labor and are heavy laden, and I will give you rest."
+  },
+  {
+    verse: "Philippians 4:13",
+    text: "I can do all things through him who strengthens me."
+  },
+  {
+    verse: "Proverbs 10:22",
+    text: "The blessing of the Lord makes rich, and he adds no sorrow with it."
   }
+  {
+    verse: "Psalm 27:1",
+    text: "Of David. The Lord is my light and my salvation; whom shall I fear? The Lord is the stronghold of my life; of whom shall I be afraid?",
+  },
+  {
+    verse: "Psalm 73:26",
+    text: "My flesh and my heart may fail, but God is the strength of my heart and my portion forever.",
+  },
 ]
